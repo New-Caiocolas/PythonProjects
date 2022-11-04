5a75054b94c9b3da488f7d57a7db2582a101b925
Caio Santos <95649452+New-Caiocolas@users.noreply.github.com>
2022-11-03 22:16:05 -0300

    add

--- 14_ESTRUTURAS DE REPETIÇÃO WHILE/ex063.py
+++ 14_ESTRUTURAS DE REPETIÇÃO WHILE/ex063.py
@@ -1,8 +1,12 @@
 t1 = 0
 t2 = 1
-cont = 1
-while cont < n:
-    n = int(input('quantos termos você deseja:'))
-    print('{} - {}'.format(t1, t2))
+cont = 3
+n = int(input('quantos termos você deseja:'))
+print('{} - {}'.format(t1, t2,), end='')
+while cont <= n:
+    t3 = t1 + t2
+    print(' - {}'.format(t3), end='')
+    t1 = t2
+    t2 = t3
     cont += 1
-    print(cont)
+print(' - FIM')
