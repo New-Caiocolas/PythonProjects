946dbb98117298806ad233e6c4e222f821e5f32a
Caio Santos <95649452+New-Caiocolas@users.noreply.github.com>
2024-08-12 20:49:10 -0300

    add

--- 18_FUNÇÕES/ex103.py
+++ 18_FUNÇÕES/ex103.py
@@ -2,6 +2,7 @@ def jogador(n="<desconhecido>",g=0):
     print(f'O nome do jogador é {n} e ele fez {g} gols:')
 nome = str(input('digite o nome:'))
 gols = str(input('digite os gols:'))
+
 if gols.isnumeric():
     gols = int(gols)
 else:
---
+++ 18_FUNÇÕES/ex104.py
@@ -0,0 +1,4 @@
+def validarNumero(n):
+    print(n)
+
+n = validarNumero("Digite um numero:")
