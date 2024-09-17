ea163f57303066032ed84f952a37b0dc992b7ade
Caio Santos <95649452+New-Caiocolas@users.noreply.github.com>
2024-09-17 20:48:58 -0300

    Update interface.py

--- PythonProjects/interface/interface.py
+++ PythonProjects/interface/interface.py
@@ -3,22 +3,29 @@ import tkinter
 window = tkinter.Tk()
 window.title('login')
 window.geometry('400x400')
-#window.configure(bg='#333333')
+window.configure(bg='#333333')
+
+def login():
+    if user_entry.get() == 'caio' and password_entry.get() == '123':
+        print('Hello world!')
+
+frame = tkinter.Frame(bg='#333333')
 
 # CRIANDO ITENS
-login_label = tkinter.Label(window, text='Login')
-user_label = tkinter.Label(window, text='Username')
-user_entry = tkinter.Entry(window)
-password_label = tkinter.Label(window, text='Password')
-password_entry = tkinter.Entry(window, show='*')
-login_button = tkinter.Button(window, text='Enter')
+login_label = tkinter.Label(frame, text='Login', bg='#333333', font='arial, 20', fg='#0094DA')
+user_label = tkinter.Label(frame, text='Username', bg='#333333', font='arial, 16', fg='#fff')
+user_entry = tkinter.Entry(frame, font='arial, 16')
+password_label = tkinter.Label(frame, text='Password', bg='#333333', font='arial, 16', fg='#fff')
+password_entry = tkinter.Entry(frame, font='arial, 16' , show='*')
+login_button = tkinter.Button(frame, text='ENTER', bg='#0094DA', font='arial, 16', fg='#fff', command=login)
 
 # MOSTRANDO ITENS NA TELA
-login_label.grid(row=1, column=1, columnspan=2)
-user_label.grid(row=2, column=1)
+login_label.grid(row=0, column=1, columnspan=2, sticky='news', pady=20)
+user_label.grid(row=2, column=1, pady=20)
 user_entry.grid(row=2, column=2)
-password_label.grid(row=3, column=1)
-password_entry.grid(row=3, column=2)
+password_label.grid(row=3, column=1, pady=20)
+password_entry.grid(row=3, column=2 )
+login_button.grid(row=4, column=1, columnspan=2, pady=20)
 
-
+frame.pack()
 window.mainloop()
