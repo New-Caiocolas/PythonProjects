1d88dd4634a6ef464c3701d8c2832930e24d4a17
new-caiocolas <caiomarcelosilvasantos@gmail.com>
2024-10-24 20:35:52 -0300

    add

--- PythonProjects/interface/interface.py
+++ PythonProjects/interface/interface.py
@@ -20,6 +20,7 @@ user_entry = tkinter.Entry(frame, font='arial, 16')
 password_label = tkinter.Label(frame, text='Password', bg='#333333', font='arial, 16', fg='#fff')
 password_entry = tkinter.Entry(frame, font='arial, 16' , show='*')
 login_button = tkinter.Button(frame, text='ENTER', bg='#0094DA', font='arial, 16', fg='#fff', command=login)
+login_button = tkinter.Button(frame, text='ENTER', bg='#0094DA', font='arial, 16', fg='#fff', command=login)
 
 # MOSTRANDO ITENS NA TELA
 login_label.grid(row=0, column=1, columnspan=2, sticky='news', pady=20)
@@ -27,6 +28,7 @@ user_label.grid(row=2, column=1, pady=20)
 user_entry.grid(row=2, column=2)
 password_label.grid(row=3, column=1, pady=20)
 password_entry.grid(row=3, column=2 )
+login_button.grid(row=4, column=1, columnspan=2, pady=20)
 login_button.grid(row=4, column=1, columnspan=2, pady=20)
 
 frame.pack()
